db6f8890486b659485b76efd3fee5e536ff96a42
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-10-01 11:28:07 +0200

    Part 1 finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Tweets from './components/Tweets'
 import RightSide from './components/RightSide'
 import defaultTweets from './assets/data/tweets.js'
 import user from './assets/data/user.js'
+import { createContext } from "react"
+
+const TweetContext = createContext()
 
 function App() {
     const [tweets, setTweets] = useState(defaultTweets)
@@ -17,11 +20,13 @@ function App() {
 
     return (
         <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
+            <TweetContext.Provider value={ {tweets, setTweets, user, theme, setTheme} }>
+                <Header />
+                <Tweets />
+                <RightSide />
+            </TweetContext.Provider>
         </div>
     )
 }
 
-export { App };
+export { App, TweetContext };
